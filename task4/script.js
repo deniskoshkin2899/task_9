a0e3e47c7762bb4e7e4e678ed31bf3c24b57697e
deniskoshkin2899 <84732627+deniskoshkin2899@users.noreply.github.com>
2021-07-18 23:14:01 +0700

    init

--- task4/script.js
+++ task4/script.js
@@ -1,6 +1,6 @@
 const smthText = document.querySelector('#smthText');
 
-smthText.addEventListener('click', function(event) {
+smthText.addEventListener('click', function() {
 
 let newText = prompt('Введите любое слово или фразу и увидите, как меняется текст ссылки');
 this.textContent = newText;
