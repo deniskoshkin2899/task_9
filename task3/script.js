a686bcf5bb11a28b5907bab1d1975be1ee1737a9
deniskoshkin2899 <84732627+deniskoshkin2899@users.noreply.github.com>
2021-07-18 21:04:43 +0700

    init

--- task3/script.js
+++ task3/script.js
@@ -6,8 +6,6 @@ function makeGreen() {
     trafficLightEl.addEventListener('click', makeYellow);
 }
 
-trafficLightEl.addEventListener('click', makeGreen);
-
 function makeYellow(){
     trafficLightEl.style.background = ('yellow');
     trafficLightEl.removeEventListener('click', makeYellow);
@@ -20,3 +18,5 @@ function makeRed(){
     trafficLightEl.removeEventListener('click', makeRed);
     trafficLightEl.addEventListener('click', makeGreen);
 }
+
+trafficLightEl.addEventListener('click', makeGreen);
